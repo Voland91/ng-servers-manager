0c4bbdb6ed80de88fab7abb0b9da3bc156348021
Mateusz Butkiewicz <Mateusz.Butkiewicz.ext@unit4.com>
2022-11-07 20:34:25 +0100

    added global styles and navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [ HttpClientModule, AngularSvgIconModule.forRoot() ]
   ],
   providers: [],
   bootstrap: [AppComponent]
